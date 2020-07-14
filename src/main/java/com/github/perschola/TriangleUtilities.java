50df3a355438587da2ae904182dfe7b151586aa0
mondira-roy2020 <mondira.roy.git2020@gmail.com>
2020-07-14 18:57:01 -0400

    triangle-tables-All tests passed

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -3,18 +3,33 @@ package com.github.perschola;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String result = "";
+        for (int i = 1; i<numberOfRows; i++){
+            result += getRow(i) + "\n";
+        }
+        return result;
+
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        //Tried another method and it works!!!
+        char[] charArray = new char[numberOfStars];
+        java.util.Arrays.fill(charArray,  '*');
+        String result = new String(charArray);
+        return (result);
+/*  A simple method      String row = "";
+                 for (int i = 0; i < numberOfStars; i++)
+                 row += "*";
+                 return row;  */
     }
 
     public static String getSmallTriangle() {
-        return null;
+        int n=5;
+        return getTriangle(n);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        int n=10;
+        return getTriangle(n);
     }
 }

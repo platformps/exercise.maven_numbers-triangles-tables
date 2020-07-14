e718da021bc5820b9db51a1ea53e993cc392f821
dmholland <kenoshatrader@gmail.com>
2020-07-14 19:25:44 -0400

    Im on triangles now

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,14 +2,38 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+
+
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder complete = new StringBuilder(1);
+        int num = 0;
+        for (int f = 1; f <= tableSize; f++) {
+            int counter = 0;
+            for (int i = 1; i <= tableSize; i++) {
+                num = i * f;
+
+                if(10>num){complete.append("  ");}
+                else if(100>num){complete.append(" ");}
+                complete.append((f * i));
+                complete.append(" ");
+                complete.append("|");
+
+            }
+                complete.append('\n');
+
+        }
+
+        return complete.toString();
     }
+
+
+
 }

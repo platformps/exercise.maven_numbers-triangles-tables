85ea50cead6e23515cb5d33177da27bb0fa35a94
mondira-roy2020 <mondira.roy.git2020@gmail.com>
2020-07-14 17:04:33 -0400

    assignment done-All tests passed

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,68 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                result += i;
+            }
+            //return result;
+        }
+        return result;
     }
 
 
+
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result="";
+        for(int i= start; i < stop; i++){
+            if(i%2 != 0){
+                result += i;
+            }
+        }
+        return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result="";
+        for(int i= start; i < stop; i+=step){
+            result += i*i;
+        }
+        return result;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String result="";
+        for(int i= 0; i < stop; i+=1){
+            result += i;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result="";
+        for(int i= start; i < stop; i+=1){
+            result += i;
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result="";
+        for(int i= start; i < stop; i+=step){
+            result += i;
+        }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result="";
+        for(int i= start; i < stop; i+=step){
+            result += (int)(Math.pow(i,exponent));
+        }
+        return result;
     }
 }

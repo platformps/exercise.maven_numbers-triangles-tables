ab32c79247f90a5d9017f14ac2eb95eeb744e986
mondira-roy2020 <mondira.roy.git2020@gmail.com>
2020-07-14 19:23:21 -0400

    tables-utilities-All tests passed

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,14 +2,27 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return (getMultiplicationTable(5));
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return (getMultiplicationTable(10));
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for (int i = 1; i <= tableSize; i++) {
+            int count = i;
+            for (int j = 1; j <= tableSize; j++)
+                if(count * j < 10)
+                    result = result + "  " + count * j + " |";
+                else if(count * j < 100)
+                    result = result + " " + count * j + " |";
+                else result = result + count*j + " |";
+            result = result + "\n";
+        }
+        return result;
     }
+
 }

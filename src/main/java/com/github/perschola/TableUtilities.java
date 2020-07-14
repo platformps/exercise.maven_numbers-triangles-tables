e60b44863d86713dbac0dff185875467f820ffaa
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-14 13:54:01 -0400

    Passed the getSmallMutipleTableTest

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,7 +2,28 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        int size = 5;
+        int start_number;
+        int muitply;
+        String rowString = "";
+        String mulitTable = "";
+
+        for(int row = 0; row < size; row++){
+            rowString = "";
+            muitply = (row + 1);
+            start_number = (row + 1);
+            for(int column = 0; column < size; column++){
+                if(start_number >= 10){
+                    rowString += " " + Integer.toString(start_number) + " |";
+                }
+                else {
+                    rowString += "  " + Integer.toString(start_number) + " |";
+                }
+                start_number += muitply;
+            }
+            mulitTable += rowString + "\n";
+        }
+        return mulitTable;
     }
 
     public static String getLargeMultiplicationTable() {

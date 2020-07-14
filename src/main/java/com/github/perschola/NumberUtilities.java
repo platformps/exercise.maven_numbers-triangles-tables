f06a3190e4cb478a44e6b34262cbc4d79ded5dfd
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-14 12:10:21 -0400

    Inital work before lunch break

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,11 +3,31 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
+        String allEvenNumbers = "";
+        while(start < stop){
+            if(start % 2 == 0){
+                allEvenNumbers += Integer.toString(start);
+                start += 2;
+            }
+            else{
+                start++;
+            }
+        }
         return null;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
+        String allOddNumbers = "";
+        while(start < stop){
+            if(start % 2 != 0){
+                allOddNumbers += Integer.toString(start);
+                start += 2;
+            }
+            else{
+                start++;
+            }
+        }
         return null;
     }
 
@@ -16,17 +36,36 @@ public class NumberUtilities {
         return null;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        int start = 0;
+        String rangeOfNums = "";
+        while(start < stop){
+            String nextNumber = Integer.toString(start);
+            rangeOfNums += nextNumber;
+            start++;
+        }
+        return rangeOfNums;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String rangeOfNums = "";
+        while(start < stop){
+            String nextNumber = Integer.toString(start);
+            rangeOfNums += nextNumber;
+            start++;
+        }
+        return rangeOfNums;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String rangeOfNums = "";
+        while(start < stop){
+            String nextNumber = Integer.toString(start);
+            rangeOfNums += nextNumber;
+            start += step;
+        }
+        return rangeOfNums;
     }
 
 

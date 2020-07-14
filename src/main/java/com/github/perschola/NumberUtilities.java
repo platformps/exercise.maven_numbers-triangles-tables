86e5716e693c4b4233c2244a77eac58099bb54b7
Calvin Quinn <caoyuru03@gmail.com>
2020-07-14 15:41:00 -0400

    finished number and table

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,66 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String s = "";
+        for(int i = start; i < stop; i++){
+            if (i % 2 == 0)
+                s = s + i;
+        }
+        return s;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String s = "";
+        for(int i = start; i < stop; i++){
+            if (i % 2 == 1)
+                s = s + i;
+        }
+        return s;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String s = "";
+        for(int i = start; i < stop; i = i + step)
+            s = s + i*i;
+        return s;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String s = "";
+        for(int i = 0; i < stop; i++){
+            s = s + i;
+        }
+        return s;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String s = "";
+        for(int i = start; i < stop; i++){
+            s = s + i;
+        }
+        return s;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String s = "";
+        for(int i = start; i < stop; i = i + step){
+            s = s + i;
+        }
+        return s;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String s = "";
+        for(int i = start; i < stop; i = i + step){
+            int n = 1;
+            for(int j = 0; j < exponent; j++)
+                n = n * i;
+            s = s + n;
+        }
+        return s;
     }
 }

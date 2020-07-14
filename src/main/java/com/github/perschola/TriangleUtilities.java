87a15cd7927205f3a91de674b885e9f7a9c047a1
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-14 13:34:31 -0400

    Finsished triangle utils and fixed test cases to match the wording given in the read me

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -3,18 +3,30 @@ package com.github.perschola;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        String rowString = "";
+        int currentRow = 1;
+        while(currentRow <= numberOfRows){
+            rowString = getRow(currentRow) + "\n";
+            triangle += rowString;
+            currentRow++;
+        }
+        return triangle;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+        for(int count = 0; count < numberOfStars; count++){
+            row += "*";
+        }
+        return row;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }

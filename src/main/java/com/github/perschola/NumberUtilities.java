01d589f89c778b63a60c3e700d53cefa7cd33cb3
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-14 13:16:04 -0400

    Finished the Number Utils all test cases passed

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -5,6 +5,7 @@ public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
         String allEvenNumbers = "";
         while(start < stop){
+
             if(start % 2 == 0){
                 allEvenNumbers += Integer.toString(start);
                 start += 2;
@@ -13,7 +14,7 @@ public class NumberUtilities {
                 start++;
             }
         }
-        return null;
+        return allEvenNumbers;
     }
 
 
@@ -28,18 +29,26 @@ public class NumberUtilities {
                 start++;
             }
         }
-        return null;
+        return allOddNumbers;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        int numSqaured;
+        String allSquaredString = "";
+       while(start < stop){
+           numSqaured = start * start;
+           allSquaredString += Integer.toString(numSqaured);
+           start += step;
+       }
+       return allSquaredString;
+
     }
 
     public static String getRange(int stop) {
         int start = 0;
         String rangeOfNums = "";
-        while(start < stop){
+        while(start != stop){
             String nextNumber = Integer.toString(start);
             rangeOfNums += nextNumber;
             start++;
@@ -69,7 +78,22 @@ public class NumberUtilities {
     }
 
 
-    public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+    public static String getExponentiations(int start, int stop, int step, int exponent){
+        String allExpoString = "";
+        int total = start;
+        while(start < stop){
+            //reset
+            int count = 1;
+            total = start;
+
+            while(count < exponent){
+                total *= start;
+                count++;
+            }
+
+            allExpoString += Integer.toString(total);
+            start += step;
+        }
+        return allExpoString;
     }
 }

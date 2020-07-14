3c306a589fd436cdd2311c948fec264c0756f633
Steven Snyder <sts12589@gmail.com>
2020-07-14 16:27:28 -0400

    even and odd challenged complete

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,12 +3,44 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String count = "";
+        if (start%2==0) {
+            for (int i = start; i < stop; i += 2) {
+                if (i < stop) {
+                    count = count + i;
+                }
+            }
+        } else {
+            start = start+=1;
+            for (int i = start; i <= stop; i += 2) {
+                if (i < stop) {
+                    count = count + i;
+                }
+            }
+
+        }
+        return count;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String count = "";
+        if (start%2!=0) {
+            for (int i = start; i < stop; i += 2) {
+                if (i < stop) {
+                    count = count + i;
+                }
+            }
+        } else {
+            start = start+=1;
+            for (int i = start; i <= stop; i += 2) {
+                if (i < stop) {
+                    count = count + i;
+                }
+            }
+
+        }
+        return count;
     }
 
 

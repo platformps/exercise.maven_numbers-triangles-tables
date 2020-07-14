a06e12e6d310d0b10d9c6559b711e9c40f2ab767
bwcsemaj <bwc_semaj@live.com>
2020-07-14 16:17:52 -0400

    added comments and fixed up some code in NumberUtilities

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -3,19 +3,32 @@ package com.github.perschola;
 import java.util.function.Function;
 
 public class TableUtilities {
+    
+    /**
+     * Produce a small multiplication table (5x5)
+     * @return String that represents a multiplication table (5x5)
+     */
     public static String getSmallMultiplicationTable() {
         return getMultiplicationTable(5);
     }
     
+    /**
+     * Produce a large multiplication table (10x10)
+     * @return String that represents a multiplication table (10x10)
+     */
     public static String getLargeMultiplicationTable() {
         return getMultiplicationTable(10);
     }
     
+    /**
+     * Produce a multiplication table (tableSize x tableSize)
+     * @return String that represents a multiplication table (tableSize x tableSize)
+     */
     public static String getMultiplicationTable(int tableSize) {
         String value = "";
         for (int index = 1; index <= tableSize; index++) {
-            value += NumberUtilities.getRange(index, (tableSize+1)*index, index, new Function<Integer, String>() {
-
+            value += NumberUtilities.getRange(index, (tableSize + 1) * index, index, new Function<Integer, String>() {
+                
                 @Override
                 public String apply(Integer integer) {
                     return String.format("%5s", (Integer.toString(integer) + " |"));
--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 
 public class TriangleUtilities {
     
+    /**
+     * Produce a right angle triangle (bottom left betting corner)
+     * @param numberOfRows the triangle will have
+     * @return a String that represents a right angle triangle
+     */
     public static String getTriangle(int numberOfRows) {
         String value ="";
         for(int index = 1; numberOfRows > index; index++){
@@ -12,14 +17,27 @@ public class TriangleUtilities {
         return value;
     }
     
+    /**
+     * Produce a line of asterisks
+     * @param numberOfStars of stars in a single row
+     * @return a String whose length is equal to numberOfStars that is filled with asterisks
+     */
     public static String getRow(int numberOfStars) {
         return String.join("", Collections.nCopies(numberOfStars, "*"));
     }
     
+    /**
+     * Produce a right angle triangle (bottom left betting corner) (5 rows)
+     * @return a String that represents a right angle triangle with 5 rows
+     */
     public static String getSmallTriangle() {
         return getTriangle(5);
     }
     
+    /**
+     * Produce a right angle triangle (bottom left betting corner) (10 rows)
+     * @return a String that represents a right angle triangle with 10 rows
+     */
     public static String getLargeTriangle() {
         return getTriangle(10);
     }

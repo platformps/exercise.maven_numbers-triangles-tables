589fc1a0abbefbd12e2f6cfa4e9ea3b61619fffd
nick-devos <nickdevos123@yahoo.com>
2020-07-14 21:11:07 -0400

    'completed'

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,57 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        if(start % 2 != 0) {
+            start += 1;
+        }
+        return getRange(start, stop, 2);
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        if(start % 2 == 0) {
+            start += 1;
+        }
+        return getRange(start, stop, 2);
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += i*i;
+        }
+
+        return result;
     }
 
-    public static String getRange(int start) {
-        return null;
+
+    public static String getRange(int stop) {
+        return getRange(0, stop, 1);
     }
+
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start, stop, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += i;
+        }
+
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += (int)Math.pow(i, exponent);
+        }
+
+        return result;
     }
 }
--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -1,15 +1,38 @@
 package com.github.perschola;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+
+        for(int i = 1; i <= tableSize; i++) {
+            result += getLine(i, tableSize);
+            result += "\n";
+        }
+
+        return result;
     }
+
+    public static String getLine(int lineNumber, int tableSize) {
+        String line = "";
+        for(int i = 1; i <= tableSize; i++) {
+            if((i*lineNumber < 10) && (i*lineNumber >= 0)) { line += "  "; }
+            else if((i*lineNumber >= 10) && (i*lineNumber < 100)) { line += " "; }
+
+            line += i*lineNumber + " |";
+        }
+        return line;
+    }
+
 }

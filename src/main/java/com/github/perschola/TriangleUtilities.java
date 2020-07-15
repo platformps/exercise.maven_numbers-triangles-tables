9e929a539cdc8f0f9d761e33c58f4527c9e20b13
Steven Snyder <sts12589@gmail.com>
2020-07-15 11:52:21 -0400

    completed triangle exercises

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -3,18 +3,27 @@ package com.github.perschola;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String result = "";
+        for (int i = 1; i < numberOfRows; i++) {
+            result = result + getRow(i) + "\n";
+        }
+        return result;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String star = "*";
+        String result = "";
+        for (int i = 0; i < numberOfStars; i++) {
+            result = result + star;
+        }
+        return result;
     }
 
     public static String getSmallTriangle() {
-        return null;
+       return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }

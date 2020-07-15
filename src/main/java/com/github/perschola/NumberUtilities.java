45585a69a265d549c8609253418db5b26fdd4807
unknown <dyirchott@hotmail.com>
2020-07-14 21:41:02 -0400

    Java Assignment update

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,59 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String numbers = "";
+        if (start % 2 == 0){
+            start= start+1;
+        }
+        for (int i = start; i < stop; i++) {
+            numbers += (i);
+            i++;
+        }
+        return numbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String numbers = "";
+        if (start % 2 == 1){
+            start= start+1;
+        }
+        for (int i = start; i < stop; i++) {
+            numbers += (i);
+            i++;
+        }
+        return numbers;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squares = "";
+        for (int i = start; i < stop; i+=5) {
+            squares += i * i;
+        }
+        return squares;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        return getExponentiations(0, stop, 1, 1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getExponentiations(start, stop, 1, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+       return getExponentiations(start, stop, step, 1);
     }
 
 
+
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String numbers = "";
+        for (int i = start; i < stop; i += step) {
+            numbers += (int)(Math.pow(i, exponent));
+        }
+        return numbers.toString();
     }
 }

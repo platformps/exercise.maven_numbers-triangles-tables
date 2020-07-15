d728a41cb8485b8ebfc4ec812406cf7c27318bda
razeroual <47251928+razeroual@users.noreply.github.com>
2020-07-15 00:58:51 -0400

    done TableUtilities

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,14 +2,51 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String toReturn = "";
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if(i*j < 10)
+                    toReturn = toReturn + "  " + j*i +" "+ "|";
+                else
+                    toReturn = toReturn + " " + j*i +" "+ "|";
+            }
+            toReturn = toReturn + "\n";
+        }
+
+        return toReturn;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String toReturn = "";
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if(i*j >= 100)
+                    toReturn = toReturn + j*i +" "+ "|";
+                else if (i*j < 100 && (i*j >= 10))
+                        toReturn = toReturn + " " + j*i +" "+ "|";
+                    else
+                        toReturn = toReturn + "  " + j*i +" "+ "|";
+            }
+            toReturn = toReturn + "\n";
+        }
+
+        return toReturn;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String toReturn = "";
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                if(i*j >= 100)
+                    toReturn = toReturn + j*i +" "+ "|";
+                else if (i*j < 100 && (i*j >= 10))
+                    toReturn = toReturn + " " + j*i +" "+ "|";
+                else
+                    toReturn = toReturn + "  " + j*i +" "+ "|";
+            }
+            toReturn = toReturn + "\n";
+        }
+
+        return toReturn;
     }
 }

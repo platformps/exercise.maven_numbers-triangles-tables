5d7b10e9485cdcd9705aabe14be6e464bdc0024c
Laura Godinez <issagodinez@Lauras-MacBook-Pro.local>
2020-07-15 00:57:46 -0400

    Changes made

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,71 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String args="";
+        for (int i=start; i <stop;i++) {
+            if (i % 2 == 0) {
+                args += i;
+            }
+        }
+        return args;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String args="";
+        for (int i=start; i <stop;i++) {
+            if (i % 2 != 0) {
+                args += i;
+            }
+        }
+        return args;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String args="";
+        for (int i=start; i < stop; i+=step){
+            args+=i*i;
+        }
+        return args;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String args="";
+        for (int i=0; i < stop; i++){
+            args+=i;
+        }
+        return args;
+
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String args="";
+        for (int i=start; i < stop; i++){
+            args+=i;
+        }
+        return args;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String args="";
+        for (int i=start; i < stop; i+=step){
+            args+=i;
+        }
+        return args;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String args="";
+        for (int i=start; i < stop; i+=step){
+            int x=1;
+            for (int y=0; y<exponent; y++){
+                x*=i;
+                args+=x;
+            }
+        }
+        return args;
     }
 }
--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,14 +2,39 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
+        String args="";
+        for (int i=1; i<=5;i++){
+            int x=i;
+            for (int y=1;y>=5; y++){
+                if (x*y<10){
+                    args+=" "+ x*y+ " |";}
+                    else {args+=" "+ x*y+ " |";}
+                    args+="\n";
+                }
+            } return args;
+        }
+
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String args="";
+
+        for (int i=1;i<=tableSize;i++){
+            int x=i;
+            for (int y=1;y<=tableSize;y++){
+                if(x*y<10) {
+                    args += " " + x * y + " |";
+                }else if (x*y<100){
+                        args+=" " + x*y+" |";
+                    args+=" \n";}
+
+
+            }
+        }
+        return args;
     }
 }

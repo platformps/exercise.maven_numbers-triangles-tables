153bf27d118e21f63d3235442344d532b7644d5c
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-07-16 19:19:51 -0400

    NumberUtilities finished

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -2,35 +2,78 @@ package com.github.perschola;
 
 
 public class NumberUtilities {
+
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                result += i;
+            }
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 1) {
+                result += i;
+            }
+        }
+        return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += i * i;
+
+        }
+
+        return result;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String result = "";
+        for (int i = 0; i < stop; i++) {
+            result += i;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            result += i;
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += i;
+        }
+        return result;
     }
+/**
+four integers: start, stop, step, exponent
+ return a String concatenation of all values, raised to the specified exponent,
+ between start up to and not including stop, incrementing by the specified step.
+
+*/
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop;  i = i+step) {
+            result = result + String.valueOf((int) Math.pow(i, exponent));
+        }
+        return result;
     }
-}
+
+
+    }

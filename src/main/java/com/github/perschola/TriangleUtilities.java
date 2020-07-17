f7fd3fd95487860d7f6e266656d6cedb0bb17d3d
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-07-16 21:47:48 -0400

    Tableutilities completed

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -9,7 +9,7 @@ public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
         String result = "";
-        for (int i = 1; i < numberOfRows; i++)
+        for (int i = 1; i <numberOfRows; i++)
             result += getRow(i) + "\n";
             return result;
         }

5646c02df0c4be294e014bd49e6b4cca09418c6b
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-07-16 21:24:14 -0400

    TriangleUtilities Completed

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -1,20 +1,51 @@
 package com.github.perschola;
 
 public class TriangleUtilities {
+    /**
+     * @param numberOfRows
+     * @return given one integer:n, returns String representation
+     * of a row of "*" whose len is equal to wid
+     */
 
     public static String getTriangle(int numberOfRows) {
-        return null;
-    }
+        String result = "";
+        for (int i = 1; i < numberOfRows; i++)
+            result += getRow(i) + "\n";
+            return result;
+        }
+
+    /**
+     * write method that returns String rep of small triangle
+     * whose base height and base width is 4 "*"
+     *
+     * @param numberOfStars
+     * @return
+     */
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String result = "";
+        for (int i = 0; i < numberOfStars; i++)
+            result += "*";
+
+        return result;
     }
+
+    /**
+     * Write a String representation oflarge Triangle,
+     * whose base height and base width is 10 "*"
+     */
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
 
+    /**
+     * Write a String representation of Triangle,
+     * whose base height and base width is equal to n.
+     */
+
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
+

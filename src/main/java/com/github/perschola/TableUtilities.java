7b69ff786e51caf1f3e8a1dc58e45268ea388747
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-07-16 20:30:43 -0400

    Tables completed

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -1,15 +1,42 @@
 package com.github.perschola;
 
+import com.sun.javadoc.SourcePosition;
+
 public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
-        return null;
+    public static String getSmallMultiplicationTable()
+    {
+        String result = "";
+        for (int i = 1; i <= 5; i++) {
+            int k = i;
+            for (int j = 1; j <= 5; j++)
+                if(k * j < 10)
+                    result = result + "  " + k * j + " |";
+                else result = result + " " + k * j + " |";
+            result = result + "\n";
+        }
+        return result;
     }
 
-    public static String getLargeMultiplicationTable() {
-        return null;
-    }
+public static String getLargeMultiplicationTable(){
+        return getMultiplicationTable(10);
+}
+
+
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        {
+            String result = "";
+            for (int i = 1; i <= tableSize; i++) {
+                int k = i;
+                for (int j = 1; j <= tableSize; j++)
+                    if(k * j < 10)
+                        result = result + "  " + k * j + " |";
+                    else if(k * j < 100)
+                        result = result + " " + k * j + " |";
+                    else result = result + k*j + " |";
+                    result=result + "\n";
+            }
+            return result;
+        }
     }
 }

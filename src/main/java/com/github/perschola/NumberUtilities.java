7b228b05fea14cd12ae16662bd4ac6ad7047366d
lkbulusu@gmail.com <luckyavailable>
2020-07-20 17:48:48 -0400

    updated numbers-tables-triangles

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,80 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+       String a = "";
+        for (int i = start; i < stop; i++) {
+            if ((i % 2) == 0) {
+                a=a.concat(String.valueOf(i));
+            }
+        }
+
+        return a;
+
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String a="";
+        for(int i=start;i<stop;i++)
+        {
+            if((i%2)!=0)
+            a=a.concat(String.valueOf(i));
+        }
+        return a;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        /** 5,10,5**/
+        String a="";
+        int square=0;
+        for(int i=start;i<stop;i+=step) {
+            square = i * i;
+            a = a.concat(String.valueOf(square));
+        }
+        return(a);
     }
 
     public static String getRange(int start) {
-        return null;
+        String a="";
+        for(int i=0;i<start-1;i++)
+        {
+            a=a.concat(String.valueOf(i));
+        }
+        return(a);
+
+
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String a="";
+        for(int i=start;i<stop;i++)
+        {
+            a=a.concat(String.valueOf(i));
+        }
+         return(a);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String a = "";
+        for (int i = start; i < stop; i += step) {
+            a = a.concat(String.valueOf(i));
+
+        }
+        return(a);
     }
 
-
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String a="";
+        for(int i=start;i<stop;i+=step)
+        {
+           double b= Math.pow(i,exponent);
+           int inum=(int) b;
+            a=a.concat(String.valueOf(inum));
+        }
+        return(a);
     }
 }

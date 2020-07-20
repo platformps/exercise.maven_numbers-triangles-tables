d7b444783e55e32e8e07f7acf81520f77a1b839a
Julia Waclawek <julie.waclawek@gmail.com>
2020-07-20 01:04:26 -0400

    Completed numbers triangles tables

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -1,15 +1,30 @@
 package com.github.perschola;
 
 public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
-        return null;
+    public static String getSmallMultiplicationTable(int tableSize) {
+        String product = "";
+            for(int i=1; i<=4; i++) {
+                tableSize = 4;
+                product = String.valueOf(i * (i++));
     }
-
-    public static String getLargeMultiplicationTable() {
-        return null;
+            return product;
+}
+    public static String getLargeMultiplicationTable(int tableSize) {
+        String product = "";
+        tableSize = 10;
+            for(int i=1; i<=tableSize; i++) {
+                product = String.valueOf(i * (i++));
+            }
+            return product;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
-    }
+        String product = "";
+        tableSize = 4;
+            for(int i=1; i<=tableSize; i++) {
+                product = String.valueOf(i * (i++));
+            }
+            return product;
+        }
+
 }

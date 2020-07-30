6caa19ef07a8f51c1e56b60d376c6cce41015c27
PhillipKinney <phillipjkinney@gmail.com>
2020-07-30 14:19:34 -0400

    update

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -1,13 +1,25 @@
 package com.github.perschola;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
-    }
 
+return null;
+
+
+    }
     public static String getRow(int numberOfStars) {
-        return null;
+        String result = "";
+
+        for (int i = 0; i < numberOfStars; i++) {
+
+            result +="*";
+
+        }
+
+        return result;
     }
 
     public static String getSmallTriangle() {
